78b858f5fcc594b4d6402462cf1b518f5cfbacc1
oleksandr_tokar <tokar.dev@gmail.com>
2022-06-07 18:14:45 +0300

    added vector extensions

--- Runtime/VectorExtension.cs
+++ Runtime/VectorExtension.cs
@@ -23,5 +23,35 @@ namespace CustomExtension
         {
             return new Vector2(Mathf.Clamp(vector.x, minValue, maxValue), Mathf.Clamp(vector.y, minValue, maxValue));
         }
+        
+        public static Vector3 BezierInterpolateV3(Vector3 p0, Vector3 c0, Vector3 p1, float t)
+        {
+            Vector3 p0c0 = Vector3.LerpUnclamped(p0, c0, t);
+            Vector3 c0p1 = Vector3.LerpUnclamped(c0, p1, t);
+
+            return Vector3.LerpUnclamped(p0c0, c0p1, t);
+        }
+
+        public static Vector2 BezierInterpolateV2(Vector2 p0, Vector2 c0, Vector2 p1, float t)
+        {
+            Vector3 p0c0 = Vector2.LerpUnclamped(p0, c0, t);
+            Vector3 c0p1 = Vector2.LerpUnclamped(c0, p1, t);
+
+            return Vector2.LerpUnclamped(p0c0, c0p1, t);
+        }
+
+
+        public static Vector3 BezierInterpolate4(Vector3 p0, Vector3 c0, Vector3 c1, Vector3 p1, float t)
+        {
+            Vector3 p0c0 = Vector3.LerpUnclamped(p0, c0, t);
+            Vector3 c0c1 = Vector3.LerpUnclamped(c0, c1, t);
+            Vector3 c1p1 = Vector3.LerpUnclamped(c1, p1, t);
+
+            Vector3 x = Vector3.LerpUnclamped(p0c0, c0c1, t);
+            Vector3 y = Vector3.LerpUnclamped(c0c1, c1p1, t);
+
+            return Vector3.LerpUnclamped(x, y, t);
+        }
+
     }
 }
