b3021c807568eb3cbf896ea1db50f7b70cc47954
tokar <tokar.dev@gmail.com>
2023-02-28 14:13:18 +0100

    added tmpro extension

---
+++ Runtime/TMProExtension.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+namespace OT.Extensions
+{
+    public static class TMProExtension
+    {
+        public static void InitDp<T>(this TMPro.TMP_Dropdown dropdown, T[] items, Action<T> listener)
+        {
+            dropdown.ClearDp();
+            dropdown.onValueChanged.AddListener(arg0 => listener?.Invoke(items[arg0]));
+            dropdown.AddOptions(items.Select(i => i.ToString()).ToList());
+        }
+
+        public static void ClearDp(this TMPro.TMP_Dropdown dropdown)
+        {
+            dropdown.onValueChanged.RemoveAllListeners();
+            dropdown.options.Clear();
+        }
+
+    }
+}
